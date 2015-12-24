ed0fb7ebd7480228c21c44289ba6a066cda63af6
William Casarin <bill@casarin.me>
2015-12-23 19:15:15 -0800

    update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ version = '0.0.1'
 setup(
     name='curlc',
     version=version,
+    packages = ['curlc'],
     description="curl wrapper to borrow cookies from your browser's authenticated session",
     author='William Casarin',
     author_email='bill@casarin.me',
@@ -12,7 +13,6 @@ setup(
     include_package_data=True,
     install_requires=[ "pycookiecheat >= 0.2.0" ],
     entry_points = {'console_scripts': ['curlc = curlc:main'] }
-    py_modules = ['curlc'],
     license="GPLv2",
     keywords='curlc',
     classifiers=[
